89e741a7913b7a772e368631b8e03d51e160ba61
urmilarawat005 <u4urmilarawat@gmail.com>
2025-01-14 17:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-//complete this code
+// Animal Class
 class Animal {
-	constructor(species){
-			this._species = species;
-	}
-	get species(){
-		return this._species;
-	}
-	makeSound(){
-		console.log(`The ${this.species} make a sound`);
-	}
+  constructor(species) {
+    this._species = species; // Private property for species
+  }
+
+  // Getter for species
+  get species() {
+    return this._species;
+  }
+
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
 }
 
-class Dog extends Animal {
-	constructor(species){
-		super(species);
-	}
-	bark(){
-		console.log("woof");	
-	}
-}
-
+// Cat Class that extends Animal
 class Cat extends Animal {
-	constructor(species){
-		super(species);
-	}
-	purr(){
-		console.log("purr")
-	}
+  constructor(species) {
+    super(species); // Call the constructor of Animal
+  }
+
+  // Method for purring
+  purr() {
+    console.log("purr");
+  }
 }
 
-const myCat = new Cat("miyu");
-myCat.makeSound();
-myCat.purr();
+// Dog Class that extends Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species); // Call the constructor of Animal
+  }
 
+  // Method for barking
+  bark() {
+    console.log("woof");
+  }
+}
 
-const myDog = new Dog("bhaw");
-myDog.makeSound();
-myDog.bark();
+// Example Usage
+
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr();      // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark();      // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
