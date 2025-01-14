be035d9921eba94c6e3a1bb8960324fb4009833f
urmilarawat005 <u4urmilarawat@gmail.com>
2025-01-14 16:34:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+			this._species = species;
+	}
+	get species(){
+		return this._species;
+	}
+	makeSound(){
+		console.log(`The ${this.species} make a sound`);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log("woof");	
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log("purr")
+	}
+}
+
+const myCat = new Cat("miyu");
+myCat.makeSound();
+myCat.purr();
+
+
+const myDog = new Dog("bhaw");
+myDog.makeSound();
+myDog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
